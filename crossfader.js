b2b63f6edf266a642147dfe2d8fadce2209b7218
elsehow <yes@cosmopol.is>
2015-08-13 18:59:05 -0700

    demos basically ready..

---
+++ crossfader.js
@@ -0,0 +1,41 @@
+$		= require('jquery')
+Kefir 		= require('kefir')
+rightKeycode	= 39
+leftKeycode	= 37
+
+var pressStream = Kefir.fromEvents(document.body, 'keydown')
+
+var leftStream = pressStream.filter(function (ev) {
+  if (ev.which == leftKeycode) return ev
+}).map(function (ev) { 
+  return -1 
+})
+
+var rightStream = pressStream.filter(function (ev) {
+  if (ev.which == rightKeycode) return ev
+}).map(function (ev) {
+  return 1
+})
+
+var posStream = Kefir.merge([leftStream, rightStream])
+
+posStream = posStream.scan(function (acc, cur) {
+  return acc += cur
+}, 50)
+
+posStream.onValue(function (pos) {
+  $('#dot').css('left', pos + '%')
+})
+
+
+var leftGain = posStream.map(function (pos) {
+  return 100 - pos
+})
+
+var rightGain = posStream.map(function (pos) {
+  return pos
+})
+
+
+var sliderclick = Kefir.fromEvents($("#dot"), 'mousedown')
+sliderclick.log()
